16a3d8d153d7126b5e142ebf274ce7d02519f3ba
Robert Hatcher <rhatcher@fnal.gov>
2018-03-09 14:04:10 -0600

    have script copy PlotUtils inc/libs into runartg4tk's UPS area

--- scripts/simple_build.sh
+++ scripts/simple_build.sh
@@ -24,4 +24,32 @@ cp ${HERE}/bootstrap_ups.sh ${MRB_INSTALL}
 set +o xtrace
 echo -e "${OUTNOCOL}"
 
+
+## how to get this automatically???
+#LIBSUB=slf6.x86_64.e15.prof
+# don't know if this will work for "debug" (ordering???)
+BUILDTYPE=`basename ${MRB_BUILDDIR} | sed -e 's/build_//g'`
+LIBSUB=`echo ${BUILDTYPE}.${MRB_QUALS} | tr ":" "."`
+
+BASEDESTDIR=${MRB_INSTALL}/runartg4tk/${RUNARTG4TK_VERSION}
+LIBDESTDIR=${BASEDESTDIR}/${LIBSUB}/lib
+if [[ ! -d ${LIBDESTDIR} ]]; then
+  echo -e "${OUTRED}missing:"
+  echo -e "  LIBDESTDIR=${LIBDESTDIR}"
+  echo -e "${OUTNOCOL}"
+else
+  echo -e "${OUTCYAN}"
+  set -o xtrace
+  mkdir -p ${BASEDESTDIR}/include/PlotUtils
+  cd  ${MRB_SOURCE}/RooMUHistos/PlotUtils
+  ls *.h
+  cp *.h ${BASEDESTDIR}/include/PlotUtils
+  rm ${BASEDESTDIR}/include/PlotUtils/LinkDef.h
+  cd ${MRB_SOURCE}/RooMUHistos/lib/
+  ls *
+  cp * ${LIBDESTDIR}
+  set +o xtrace
+  echo -e "${OUTNOCOL}"
+fi
+
 # end-of-script
